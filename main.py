98ef50520d7817c394754b99bb8f7045694c170e
JamieMiles99 <136345170+JamieMiles99@users.noreply.github.com>
2026-06-10 20:11:12 +0100

    Update main.py
    Replaced with rain alert project

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+import requests
+from twilio.rest import Client
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+client = Client(account_sid, auth_token)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+api_key = os.environ.get("OWM_API_KEY")
+latitude = 53.383
+longitude = -1.4659
+records_needed = 4
+
+parameters = {
+    "lat": latitude,
+    "lon": longitude,
+    "cnt": records_needed,
+    "appid": api_key
+}
+
+# lat={lat}&lon={lon}&appid={API key}
+
+api_response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+api_response.raise_for_status()
+forecast_data = api_response.json()
+print(forecast_data)
+print(api_response.status_code)
+
+#Get the list of updates from json response
+time_periods = forecast_data["list"]
+#Iterate through the list to check for code <700 and set rain to True
+rain_forecasted = False
+for period in time_periods:
+    weather_records = period["weather"]
+    for record in weather_records:
+        print(f"Weather code {record["id"]}")
+        if record["id"] < 700:
+            rain_forecasted = True
+
+if rain_forecasted:
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        to='whatsapp:+447795387771',
+        body="It's going to rain in the next 12 hours. Bring an ☂️",
+    )
+else:
+    print("No rain forecast in next 12 hours")
